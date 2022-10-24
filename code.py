99cb7a097ef61c5560731a0604504ed212b9a6b3
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2022-10-24 15:33:05 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by Cristiano Sellitto
+# Created in October 2022
+# A file for the "Space Aliens" game for CiruitPython
+
+
+import ugame
+import stage
+
+def game_scene():
+    # Finds if a number is associated with a month
+
+    print("\n\n\n")
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    game_scene()
