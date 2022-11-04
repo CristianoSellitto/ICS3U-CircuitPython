3e3528989466a016100b1b8d74685c2883dca69c
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2022-11-04 10:36:40 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Cristiano Sellitto
+# Created in October 2022
+# A file for the contants for the "Space Aliens" game for CiruitPython
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
