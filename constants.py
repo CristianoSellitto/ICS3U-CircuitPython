cbcab262c74c34e98da5749a6631b25e848177c9
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2022-11-10 11:16:34 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@
 # Created in October 2022
 # A file for the contants for the "Space Aliens" game for CiruitPython
 
+# Constants
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -12,9 +13,14 @@ SPRITE_SIZE = 16
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
+# Button States
 button_state = {
     "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
     "button_released": "released",
 }
+
+# Palettes
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
